47f4d093c5f03a7b7b05fafd485ee9eca741b27e
Joao Albuquerque <joaopealbuquerque1@gmail.com>
2025-01-25 16:45:15 -0300

    adicionando chamada ao servico de email

--- src/main/java/com/ms/email/consumers/EmailConsumer.java
+++ src/main/java/com/ms/email/consumers/EmailConsumer.java
@@ -2,13 +2,18 @@ package com.ms.email.consumers;
 
 import com.ms.email.dtos.EmailRecordDto;
 import com.ms.email.models.EmailModel;
+import com.ms.email.services.EmailService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
 public class EmailConsumer {
+
+    @Autowired
+    EmailService service;
 
     //Ouvinte da fila lá no AMQP cloud
     @RabbitListener(queues = "${broker.queue.email.name}") //Via propriedade o código entende que tem que ouvir esta fila
@@ -17,5 +22,6 @@ public class EmailConsumer {
         BeanUtils.copyProperties(emailDto, emailModel);
 
         //Enviar o email
+        service.enviarEmail(emailModel);
     }
 }
